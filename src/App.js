3334fd1d8c55c79b23331973311af8a8cc584bf0
abhhinav <abhinavkmr86@gmail.com>
2020-09-21 12:58:26 +0530

    Counter and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Layout from './pages/layout.js';
+
+import {
+Link,
+NavLink,
+BrowserRouter as Router,
+Switch,
+Route
+ } from 'react-router-dom';
+
+import Counter from './components/counter.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Layout />
+        
+          <div className="container-fluid">
+            <Switch>
+              <Route path="/" exact>
+                <h1>Hello React</h1>
+              </Route>
+              <Route path="/counter" >
+                <Counter />
+              </Route>
+            </Switch>
+          </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/counter.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react';
+
+export default function Counter() {
+    //alert("Function called!");
+    // const counterResult = useState(0);
+    // const counter = counterResult[0];
+    // const setCounter = counterResult[1];
+    
+    const [counter, setCounter ] = useState(0) ; // [stateValue, updaterFunction]
+    const [error, setError] = useState("");
+
+    function onIncrement () {
+      //counter = counter + 1;   // WILL NOT WORK
+      if(counter ==0){
+          setError("");
+      }
+      setCounter(counter + 1);
+    }
+    
+     function onDecrement () {
+      //counter = counter + 1;   // WILL NOT WORK
+      if (counter == 0){
+        setError("Counter can't be Negative!");
+        return;
+      }
+      setCounter(counter - 1);
+    }
+    
+    return (
+      <div className="container">
+        <h1>Counter</h1>
+        <div className="bg-danger">
+            {error}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button onClick={onDecrement} className="btn btn-primary">-</button>
+          {counter}
+          <button onClick={onIncrement} className="btn btn-secondary">+</button>
+        </div>
+      </div>
+    )
+  }
+  
+  
+  //const root  = document.querySelector("#root");
+  //ReactDOM.render(<Counter/>, root);
